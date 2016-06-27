d477992d6327af652d9522b02ebfebf134d7739d
jiaxin <yjx787172225@qq.com>
2016-06-27 12:51:09 +0800

    3.修改 spring boot 启动banner（我只想证明我是有多闲）

--- src/com/sirun/util/Application.java
+++ src/com/sirun/util/Application.java
@@ -5,6 +5,7 @@ import java.security.ProtectionDomain;
 import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.ResolvableType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,37 @@ public class Application {
 	String home(){
 		return "hello world";
 	}
-	
+	/**
+	 * Spring Boot运行方法
+	 * 
+	 * 1.
+	 */
 	public static void main(String[] args){
 		SpringApplication.run(Application.class,args);
+	}
+	
+	/**
+	 * Spring Boot运行方法
+	 * 
+	 * 2. 关闭banner
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public void startSpringBoot_2(){
+		SpringApplication application = new SpringApplication(Application.class);
+		application.setShowBanner(false);//关闭banner
+		application.run();
+	}
+	
+	/**
+	 * Spring Boot运行方法
+	 * 
+	 * 3. 修改banner
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public void startSpringBoot_3(){
+		new SpringApplicationBuilder().showBanner(true).sources(Application.class).run();
 	}
 	
 	@Test
